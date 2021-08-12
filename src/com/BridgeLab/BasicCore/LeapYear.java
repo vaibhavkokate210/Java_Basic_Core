c6375c56ae6844b028ed7a33757d55f73d05f20e
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-12 10:24:36 +0530

    leap year done

---
+++ src/com/BridgeLab/BasicCore/LeapYear.java
@@ -0,0 +1,18 @@
+package com.BridgeLab.BasicCore;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the year ");
+		int year=sc.nextInt();
+		 if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+		     System.out.println(year+" is Leap year");
+		 else
+			 System.out.println(year+" is not Leap year");
+	}
+
+}
