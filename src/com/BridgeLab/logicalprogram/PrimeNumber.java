e5d3f5880a24ac28c7d83dffaeae404a3de5f33b
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-13 09:12:07 +0530

    prime number completed

---
+++ src/com/BridgeLab/logicalprogram/PrimeNumber.java
@@ -0,0 +1,30 @@
+package com.BridgeLab.logicalprogram;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		int i,flag=0,n;
+		System.out.println("Enter number to check prime or not :");
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		  if(n==0||n==1)  
+		   System.out.println(n+" is not prime number");      
+		  else
+		  {  
+		    for(i=2;i<=n/2;i++)
+		    {      
+		       if(n%i==0)
+		       {      
+		           System.out.println(n+" is not prime number");      
+		           flag=1;      
+		           break;      
+		       }      
+		     }      
+		   if(flag==0) 
+		   System.out.println(n+" is prime number");
+		  }  
+	}
+
+}
